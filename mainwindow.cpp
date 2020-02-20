a43b387f5908acf1f7869a97b17bcb6cc90e43b3
waleramir <42164244+waleramir@users.noreply.github.com>
2020-02-20 20:55:04 +0300

    Done what's written in the description
    Логика из описания выполнена, теперь жду реальное задание от вас

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,3 +11,52 @@ MainWindow::~MainWindow()
 {
   delete ui;
 }
+
+enum class Destination
+{
+  Asia = 0,
+  Europe,
+  NorthAmerica,
+  SouthAmerica,
+  Africa,
+  Australia
+};
+
+enum class Transport
+{
+  Sea = 0,
+  Ground,
+  Air
+};
+
+void MainWindow::on_PricePushButton_clicked()
+{
+  auto to = Destination(ui->FromComboBox->currentIndex());
+  auto from = Destination(ui->ToComboBox->currentIndex());
+  auto transport = Transport(ui->TransportComboBox->currentIndex());
+
+  if (from == to)
+    ui->PriceLabel->setNum(0);
+  else
+  {
+    // allsolvingformula
+    auto price = (int(to) * 10 + int(from) * 10) * (1 + int(transport));
+    ui->PriceLabel->setNum(price);
+  }
+}
+
+void MainWindow::on_FreightPushButton_clicked()
+{
+  switch (Transport(ui->TransportComboBox->currentIndex()))
+  {
+    case Transport::Air:
+      ui->FreightLabel->setText("Plane is ready");
+      break;
+    case Transport::Sea:
+      ui->FreightLabel->setText("Ship is ready");
+      break;
+    case Transport::Ground:
+      ui->FreightLabel->setText("Truck is ready");
+      break;
+  }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -4,18 +4,27 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
+
+private slots:
+
+  void on_PricePushButton_clicked();
+
+  void on_FreightPushButton_clicked();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow* ui;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
